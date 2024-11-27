8c4b90954763b8b7973a62191a72756b8bf53ae8
Rafsan12 <rafsan.web.2022@gmail.com>
2024-11-27 20:50:25 +0600

    Partners update

--- src/component/page/Partners.jsx
+++ src/component/page/Partners.jsx
@@ -1,11 +1,11 @@
 import { partnerLogo } from "../../../lib/data";
-import Containerbox from "../material/Containerbox";
+import ContainerBox from "../../material/Containerbox";
 
 const Partners = () => {
   return (
     <>
       <section className="bg-[#07292F] px-3">
-        <Containerbox className={"max-w-3xl mx-auto pb-10 pt-8"}>
+        <ContainerBox className={"max-w-3xl mx-auto pb-10 pt-8"}>
           <h4 className="text-center text-base font-normal text-[#0FF1F6] uppercase mb-4 lg:pb-6">
             our trusted partners
           </h4>
@@ -13,11 +13,15 @@ const Partners = () => {
           <div className=" flex-wrap flex items-center gap-x-5 justify-center  ">
             {partnerLogo.map((logo, i) => (
               <div key={i} className="max-w-full">
-                <img src={logo} className="max-w-full object-cover" alt="svg.img" />
+                <img
+                  src={logo}
+                  className="max-w-full object-cover"
+                  alt="svg.img"
+                />
               </div>
             ))}
           </div>
-        </Containerbox>
+        </ContainerBox>
       </section>
     </>
   );

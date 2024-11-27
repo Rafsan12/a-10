8d3bb77ef732f7c89cf06c123f62aba7452124b5
Rafsan12 <rafsan.web.2022@gmail.com>
2024-11-27 20:53:19 +0600

    Embrace update

--- src/component/page/Embrace.jsx
+++ src/component/page/Embrace.jsx
@@ -1,12 +1,12 @@
-import Containerbox from "../material/Containerbox";
-import Signupbutton from "../material/Signupbutton";
 import React from "react";
+import ContainerBox from "../../material/Containerbox";
+import SignupButton from "../../material/Signupbutton";
 class Embrace extends React.PureComponent {
   render() {
     return (
       <>
         <section className="px-3 py-20 lg:py-24">
-          <Containerbox>
+          <ContainerBox>
             <div className="space-y-[23px] text-center grid place-items-center ">
               <p className="text-lg font-normal text-[#0FF1F6] font-montserrot uppercase">
                 get started
@@ -17,12 +17,15 @@ class Embrace extends React.PureComponent {
                 <span className="font-medium italic">outbound.</span>
               </h2>
               <p className="text-center text-white/75 font-[300] text-lg font-worksans ">
-                Wizia lets you train, activate, and optimize aiDRs.<br/> Take your 
-                outbound to new levels of performance and efficiency.
+                Wizia lets you train, activate, and optimize aiDRs.
+                <br /> Take your outbound to new levels of performance and
+                efficiency.
               </p>
-              <Signupbutton className={"md:w-[30%] lg:w-[22%] px-0"} >Sign up for the demo</Signupbutton>
+              <SignupButton className={"md:w-[30%] lg:w-[22%] px-0"}>
+                Sign up for the demo
+              </SignupButton>
             </div>
-          </Containerbox>
+          </ContainerBox>
         </section>
       </>
     );
